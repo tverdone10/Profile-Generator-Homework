02f68b8d255049eda8315dcef4f2036829dc14bf
tverdone <tverdone10@gmail.com>
2019-11-19 17:29:38 -0800

    gets on the page and makes a pdf

--- Profile-Generator-Hw/index.js
+++ Profile-Generator-Hw/index.js
@@ -1,35 +1,75 @@
 var inquirer = require("inquirer");
-var fs = require('fs');
+var fs = require("fs");
+var generateHTML = require("./generateHTML");
+var axios = require("axios");
+var path = require("path")
+var pdf = require("html-pdf")
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What is your GitHub Profile?"
-  },
-  {
-    type: "input",
-    name: "name2",
-    message: "What is your name?"
-  },
-  {
-    type: "input",
-    name: "name3",
-    message: "What is your pet?"
-  },
+const questions = [
+  "What is your GitHub username",
+  "What's your favorite color"
+];
 
-
-]).then(function(data) {
-
-  var filename = data.name.toLowerCase().split(' ').join('') + ".html";
-
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-    if (err) {
-      return console.log(err);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: questions[0]
+    },
+    {
+      type: "list",
+      name: "color",
+      choices: ["blue", "green", "red", "pink"],
+      message: questions[1]
     }
+  ])
+  .then(function(data) {
+    console.log(data.name);
 
-    console.log("Success!");
-
+    axios
+      .get("https://api.github.com/users/" + data.name)
+      .then(function(response) {
+        console.log(response.data);
+        // find the stars
+        axios
+          .get("https://api.github.com/users/" + data.name + "/repos")
+          .then(function(repos) {
+            console.log("--->", repos.data);
+            var stars = 0;
+            for (i = 0; i < repos.data.length; i++) {
+              //console.log(repos.data[i].stargazers_count)
+              stars = stars + repos.data[i].stargazers_count;
+              // console.log(stars)
+            }
+            console.log(stars);
+            // then you generate the html
+            var color = data.color;
+            var html =generateHTML({ color, stars, ...response.data })
+            console.log(html)
+            fs.writeFileSync(path.join(process.cwd(), "resume.html"), html);
+            generatePDF(html)
+          });
+      });
   });
-});
+
+
+  function generatePDF(html){
+  pdf.create(html).toFile("./resume.pdf", function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
+}
+  
+// var filename =
+//   data.name
+//     .toLowerCase()
+//     .split(" ")
+//     .join("") + ".html";
+
+// fs.writeFile(filename, JSON.stringify(data, null, "\t"), function(err) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log("Success!");
